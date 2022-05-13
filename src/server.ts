b0ef717b578f2ab0a84baf3bb4ba675440d44c46
Marceometry <marceometry@gmail.com>
2022-05-13 00:32:24 -0300

    feat: increase payload size limit

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3333
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
 app.use(routes)
 
 app.listen(port, () => {
